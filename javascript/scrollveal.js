a593e7649ba92cb38be0c687676f74285c922d83
Portego00 <98835194+Portego00@users.noreply.github.com>
2023-02-20 12:53:45 -0500

    Add files via upload

---
+++ javascript/scrollveal.js
@@ -0,0 +1,75 @@
+const defaultProps = {
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  distance: "30px",
+  origin: "bottom",
+  duration: 1000,
+  desktop: true,
+  mobile: true,
+};
+// Hero Section
+ScrollReveal().reveal(".hero-title", {
+  ...defaultProps,
+  delay: 200,
+  origin: window.innerWidth > 768 ? "left" : "bottom",
+});
+ScrollReveal().reveal(".name", {
+  ...defaultProps,
+  distance: "0px",
+  delay: 400,
+  scale: 0,
+});
+ScrollReveal().reveal(".hero-cta", {
+  ...defaultProps,
+  delay: 700,
+  origin: window.innerWidth > 768 ? "left" : "bottom",
+});
+ScrollReveal().reveal(".scroll-down-link", {
+  ...defaultProps,
+  delay: 1000,
+  origin: "top",
+});
+
+/* About Section */
+ScrollReveal().reveal(".section-title", {
+  ...defaultProps,
+  delay: 100,
+  distance: "0px",
+});
+ScrollReveal().reveal(".about-wrapper__image", {
+  ...defaultProps,
+  delay: 300,
+  scale: 0.5,
+});
+
+ScrollReveal().reveal(".about-wrapper__info", {
+  ...defaultProps,
+  delay: 500,
+  distance: "100px",
+  origin: window.innerWidth > 768 ? "left" : "top",
+});
+
+/* Projects Section */
+ScrollReveal().reveal(".project-wrapper__text", {
+  ...defaultProps,
+  delay: 100,
+  origin: window.innerWidth > 768 ? "left" : "bottom",
+});
+
+ScrollReveal().reveal(".project-wrapper__image", {
+  ...defaultProps,
+  delay: 100,
+  scale: 0.9,
+  origin: window.innerWidth > 768 ? "right" : "bottom",
+});
+
+/* Contact Section */
+ScrollReveal().reveal(".contact-wrapper", {
+  ...defaultProps,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".fa", {
+  ...defaultProps,
+  delay: 100,
+  interval: 100,
+});
